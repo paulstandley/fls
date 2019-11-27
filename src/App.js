f0f561b8c804f5a10b65180c158538af0cab5abd
Paul Standley <paulstandley1972@gmail.com>
2019-11-27 22:45:40 +0000

    resart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Hay</h1>
-      </header>
+      <Header />
     </div>
   );
 }
